c07ae13540c6f3844bc7a8af979e68c9b3a33b39
wudu98 <1150269136@qq.com>
2023-06-08 16:06:23 +0800

    0608 1606

--- benchmark/batch_gemm_benchmark.c
+++ benchmark/batch_gemm_benchmark.c
@@ -32,9 +32,7 @@ double fp_peak(){
 	return Gflops;
 }
 
-void my_blas_batch_sgemm(const int batch_count, const int *batch_size, const int *batch_head, const CBLAS_LAYOUT layout, const CBLAS_TRANSPOSE transa, const CBLAS_TRANSPOSE transb, const int* m, const int* n, const int* k, const float* alpha, const float ** a, const int* lda, const float ** b, const int* ldb, const float* beta, float ** c, const int* ldc){
-	printf("pre_loda Error!");
-}
+void my_blas_batch_sgemm(const int batch_count, const int *batch_size, const int *batch_head, const CBLAS_LAYOUT layout, const CBLAS_TRANSPOSE transa, const CBLAS_TRANSPOSE transb, const int* m, const int* n, const int* k, const float* alpha, const float ** a, const int* lda, const float ** b, const int* ldb, const float* beta, float ** c, const int* ldc);
 
 
 int main(int argc, char *argv[]){
